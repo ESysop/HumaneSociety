6df372989a25a4e4d972d182406d458bfb5fc1c5
esysop <leehassel@gmail.com>
2016-04-04 18:23:27 -0500

    Last commit for today Working on menu system.

--- HumaneSociety1/AdoptionDesk.cs
+++ HumaneSociety1/AdoptionDesk.cs
@@ -8,6 +8,7 @@ namespace HumaneSociety1
 	{
 	class AdoptionDesk
 		{
+		OrphanedAnimal inboundAnimal = new OrphanedAnimal();
 		public AdoptionDesk ()
 			{
 
@@ -17,10 +18,27 @@ namespace HumaneSociety1
 			{
 			Console.WriteLine("\tHUMANE SOCIETY SOLUTION CENTER\n\n\t\tMAIN MENU\n");
 			}
+
 		public void MenuChoiceScreen ()
 			{
 			Console.WriteLine("Choose from the list below:\n\n[1] \tNew Animal\n[2]\tNew Adoption\n[3]\tCheck Animal Inventory\n[4]\tList Adopter History");
+			string menuChoice = Console.ReadLine();
+			switch (menuChoice)
+				{
+				case "1":
+					inboundAnimal.IntakeAnimalType();
+					break;
+				case "2":
+
+					break;
+				case "3":
+
+				default:
+					break;
+
+				}
 			}
 
 		}
 	}
+	
