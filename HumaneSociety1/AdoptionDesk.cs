f210c6aab6fa79f6fd835d8148bb790c72a53982
esysop <leehassel@gmail.com>
2016-04-07 13:43:22 -0500

    Commit to clean up at this point.

--- HumaneSociety1/AdoptionDesk.cs
+++ HumaneSociety1/AdoptionDesk.cs
@@ -8,6 +8,7 @@ namespace HumaneSociety1
 	{
 	class AdoptionDesk
 		{
+		public List<string> adopterNameList = new List<string>();
 		public List<Cage> cages;
 
 		OrphanedAnimal inboundAnimal;
@@ -35,18 +36,18 @@ namespace HumaneSociety1
 					MenuChoiceScreen();
 					break;
 				case "2":
-					adopter.adoptionMenu();
+					adoptionMenu();
 					adopter = new Adopter();
 
 					break;
 				case "3":
 					//check animal inventory
 					break;
+					//adopter history list
 				case "4":
-					//adopter history
 					break;
 				case "5":
-					//List of food needs
+				//List of food needs
 
 				default:
 					MenuChoiceScreen();
@@ -54,7 +55,28 @@ namespace HumaneSociety1
 
 				}
 			}
-
+			public void adoptionMenu ()
+				{
+				Console.WriteLine("Thanks for giving an old friend a new home!\n\nFor our records we require your name, please enter it below.\n\n");
+				string adopterName = Console.ReadLine();
+				adopterNameList.Add(adopterName);
+				Console.WriteLine("\nWhat kind of animal are you adopting?\n\n[1]\tBird\n[2]\tCat\n[3]\tDog");
+				string animalType = Console.ReadLine();
+					if (animalType == "1")
+						{
+						//add to birdlist
+						}
+					else if (animalType == "2")
+						{
+						//add to catlist
+						}
+					else if (animalType == "3")
+						{
+						//add to doglist
+						}
+				}
 		}
+
 	}
+	
 	
